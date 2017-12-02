03dd2b9c6f5ef3615718b1be30002c16c1fd3ad3
Eagle <Eagle@10.96.199.187>
2017-12-02 09:56:12 +0800

    Init

---
+++ src/main/java/devutility/test/dubbo/client/App.java
@@ -0,0 +1,17 @@
+package devutility.test.dubbo.client;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.devutility.test.dubbo.common.interfaces.IUserService;
+import com.devutility.test.dubbo.common.models.User;
+
+public class App {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
+		context.start();
+		IUserService userService = (IUserService) context.getBean("userService");
+		User user = userService.getUser("Aldwin");
+		System.out.println("Hello world" + user.getName());
+	}
+}
